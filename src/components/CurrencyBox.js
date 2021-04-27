791e3a67acd161f6a49baf5f455ccbd00eb4124f
Jose-cod7 <josegarriaga7@gmail.com>
2021-04-27 19:53:55 +0200

    css-changes

--- src/components/CurrencyBox.js
+++ src/components/CurrencyBox.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Col, Image } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBold, faSortUp } from "@fortawesome/free-solid-svg-icons";
+import { faSortUp } from "@fortawesome/free-solid-svg-icons";
 
 const CurrencyBox = (props) => {
   return (
-    <Col xs lg="3" className="column-crypto">
+    <Col md="2" className="column-crypto">
       <div className="head-crypto">
         <Image className="logo-crypto" src={props.logo} />
         <h3 className="coin-name">{props.name}</h3>
@@ -20,13 +20,13 @@ const CurrencyBox = (props) => {
             <FontAwesomeIcon
               className="fa-lg "
               icon={faSortUp}
-              style={{ color: "#2FF44D " }}
+              style={{ color: "#6fbf7b" }}
             />
           </div>
           <div>
             <span
               className="number-percent"
-              style={{ color: "#2FF44D ", fontSize: "20px" }}
+              style={{ color: "#6fbf7b", fontSize: "20px" }}
             >
               + 16%
             </span>
